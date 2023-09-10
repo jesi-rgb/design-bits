74c7a77bac73b527be2476f6aa97db4bfc39d7d0
Jesús Enrique Rascón <blograso@gmail.com>
2023-09-10 13:59:54 +0200

    yeah

--- fonts/alphabet_ct.py
+++ fonts/alphabet_ct.py
@@ -8,7 +8,14 @@ from coldtype import *
 from coldtype.warping import warp
 
 factor = 20
-tl = Timeline(26 * factor, fps=60)
+length = 86
+tl = Timeline(length * factor, fps=60)
+
+font = Font.Cacheable("~/fonts/variable/Mona-Sans.ttf")
+
+
+print(sys.maxunicode)
+codes = list(range(sys.maxunicode))
 
 
 @animation(rect=(1920, 1080), timeline=tl, composites=1, bg=DB_BLACK)
@@ -20,16 +27,16 @@ def render(f):
         .ch(warp(f.i * 30, f.i, mult=100)),
         P(
             StSt(
-                chr(65 + f.i // factor % 26),
-                Font.MutatorSans(),
+                chr(65 + f.i // 10),
+                font,
                 f.e(e, r=(500, 750)),
                 wdth=1 - pe,
                 wght=pe,
                 ro=1,
             )
-            .fssw(-1, DB_LIGHT_GREEN, 3)
+            .fssw(-1, DB_LIGHT_GREEN, 5)
             .align(f.a.r, tx=1)
             .insert(0, f.last_render())
-            .insert(1, P(f.a.r).f(DB_BLACK, 0.05))
+            .insert(1, P(f.a.r).f(DB_BLACK, 0.1))
         ),
     ]
