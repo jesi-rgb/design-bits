ad5269fd35f828de0c21aa19edf3886995adf192
Jesús Enrique Rascón <blograso@gmail.com>
2023-09-13 17:06:55 +0200

    alphabet

--- fonts/alphabet_ct.py
+++ fonts/alphabet_ct.py
@@ -14,29 +14,37 @@ tl = Timeline(length * factor, fps=60)
 font = Font.Cacheable("~/fonts/variable/Mona-Sans.ttf")
 
 
-print(sys.maxunicode)
-codes = list(range(sys.maxunicode))
+video_scale = 2
 
 
-@animation(rect=(1920, 1080), timeline=tl, composites=1, bg=DB_BLACK)
-def render(f):
-    pe = f.e(e := "qeio", 2)
+@animation(
+    rect=(1920 * video_scale, 1080 * video_scale),
+    timeline=tl,
+    composites=1,
+    bg=DB_BLACK,
+)
+def alphabet(f):
+    pe = f.e(e := "qeio", 8)
     return [
-        P(f.a.r.inset(70))
-        .fssw(-1, DB_LIGHT_GREEN, 2, 1)
-        .ch(warp(f.i * 30, f.i, mult=100)),
+        # P(f.a.r.inset(70))
+        # .fssw(-1, DB_LIGHT_GREEN, 2, 1)
+        # .ch(warp(f.i * 30, f.i, mult=100)),
         P(
             StSt(
-                chr(65 + f.i // 10),
+                chr(65 + f.i // 20),
                 font,
-                f.e(e, r=(500, 750)),
+                f.e(e, r=(1300, 1700)),
                 wdth=1 - pe,
                 wght=pe,
                 ro=1,
             )
-            .fssw(-1, DB_LIGHT_GREEN, 5)
+            .fssw(-1, DB_LIGHT_GREEN, 2)
             .align(f.a.r, tx=1)
-            .insert(0, f.last_render())
-            .insert(1, P(f.a.r).f(DB_BLACK, 0.1))
-        ),
+        )
+        .insert(0, f.last_render())
+        .insert(1, P(f.a.r).f(DB_BLACK, 0.1)),
     ]
+
+
+def release(passes):
+    FFMPEGExport(alphabet, passes).prores().write().open()
